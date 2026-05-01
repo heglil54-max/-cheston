7b5c2e59d9a8988c1ff4c0a61d2946ed1f26ac2d
heglil54-max <heglil54@gmail.com>
2026-05-02 01:01:25 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,10 +1,1 @@
--- CHETSON AI BRIDGE
-local success, code = pcall(function() 
-    return game:HttpGet("https://pastebin.com") 
-end)
-
-if success then
-    loadstring(code)()
-else
-    warn("Ошибка связи с ИИ-облаком")
-end
+loadstring(game:HttpGet("https://pastebin.com"))()
